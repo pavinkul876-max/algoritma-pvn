f3d6ed4034fe4bf1957a14f546330b0534ae38fc
pavinkul876-max <pavinkul876@gmail.com>
2026-08-05 08:22:05 +0700

    Add files via upload

---
+++ algoritma.pavin/pavin.py
@@ -0,0 +1,4 @@
+print("haloo dunia ")
+print("Nama Saya PAVITO KEVIN I.R.")
+print("Versi 3.X")
+print("usia aku :",16,"Tahun")
---
+++ algoritma.pavin/program_pertama.py
@@ -0,0 +1,23 @@
+#Program Pertamaku
+#Nama:Pavito Kevin Imanu Rafif
+#Kelas:X RPL 2
+
+print("================================================")
+print("         SELAMAT  DATANG  DI  PYTHON!")
+print("================================================")
+print()
+print("Program ini di buat oleh:", "Pavito Kevin Imanu Rafif")
+print("SEKOLAH SMK TJP Tuban")
+print("Tahun    :", 2026)
+print()
+print(10    +   5)
+print(20    -   8)
+print(15    /   4)
+print(15    //  4)
+print(4*7)
+print(2**8)
+a=27
+b=4
+print(a+b)
+print(a-b)
+print()
