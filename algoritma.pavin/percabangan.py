6617f2a335c5b61fad98f81ba07f3f0eda6cff4b
pavinkul876-max <pavinkul876@gmail.com>
2026-08-26 07:23:28 +0700

    Add files via upload

---
+++ algoritma.pavin/percabangan.py
@@ -0,0 +1,11 @@
+nilai=int(input("masukan nilai :"))
+angka=int(input("masukan angka :"))
+if nilai >= 75:
+    print("lulus")
+else:
+    print("tidak lulus:")
+
+if angka % 5 == 0:
+    print("Genap")
+else:
+    print("Ganjil")
