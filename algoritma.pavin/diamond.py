c2f6bf1eb6e72fa26bd8bc6cc1f3ba2e4632e03e
pavinkul876-max <pavinkul876@gmail.com>
2026-09-02 11:55:48 +0700

    Add files via upload

---
+++ algoritma.pavin/diamond.py
@@ -0,0 +1,11 @@
+n=9
+for i in range(1, n + 1):
+    spasi= " " * (n - i + 2)
+    starpvn= "*" * (2 * i -1)
+    print(spasi + starpvn)
+print("PAVIN NI BOSSSSSSSSS")
+for i in range(n, 0, -1):
+    spasi= " " * (n - i + 2)
+    starpvn= "*" * (2 * i -1)
+    print(spasi + starpvn)
+    
