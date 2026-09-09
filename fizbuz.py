cccb7f7a76aceb3cbd115920fcccc11ce3255f8b
pavinkul876-max <pavinkul876@gmail.com>
2026-09-09 09:21:48 +0700

    Add files via upload

---
+++ fizbuz.py
@@ -0,0 +1,12 @@
+n= int(input("Masukan Angka :"))
+i=1
+while i <=n:
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0 :
+        print("Fizzz")
+    elif i % 5 == 0 :
+            print("Buzz")
+    else:
+        print(i)
+    i+=1
